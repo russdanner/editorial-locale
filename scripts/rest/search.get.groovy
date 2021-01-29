a8e6f90979774099e4f68a08c17fd139836ac451
admin admin <evaladmin@example.com>
2021-01-28 20:52:16 -0500

    User admin wrote content /scripts/rest/search.get.groovy

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -16,11 +16,13 @@
 
 import org.craftercms.sites.editorial.SearchHelper
 
+
 def userTerm = params.userTerm
 def categories = params["categories[]"]
 def start = params.start ? params.start as Integer : 0
 def rows = params.rows ? params.rows as Integer : 10
-def searchHelper = new SearchHelper(elasticsearch, urlTransformationService)
+def locale = params.locale ? params.locale : "en"
+def searchHelper = new SearchHelper(elasticsearch, urlTransformationService, locale)
 def results = searchHelper.search(userTerm, categories, start, rows)
 
 return results;
